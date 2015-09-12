2e616de46101c339e460ba9a7db613c153a4acd7
Darwin Smith II <dwin@dlsmi.com>
2015-09-12 00:48:03 -0700

    Added timing of arrangement

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,7 +1,9 @@
 # Bubble Sort - Darwin Smith II - 2015-9-9
-import random
+import random, datetime
 theList = []
 number = 100
+start = 0
+stop = 0
 #Populate list with 100 random values 1-1000
 while number > 0:
 	theList.append(random.randint(1, 1000))
@@ -10,8 +12,11 @@ while number > 0:
 print 'Before:', theList ,'\n\n'	
 
 for numerals in range(len(theList)-1,0,-1):
+	if start == 0:
+		start = datetime.datetime.now()
         for i in range(numerals):
             if theList[i]>theList[i+1]:
                 theList[i],theList[i+1]=theList[i+1],theList[i] 
-
+stop =  datetime.datetime.now()
 print 'After:', theList
+print '\nArrange Time:', (stop - start)
