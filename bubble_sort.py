ce9290981bf276afbe276d0924db58e3112069c3
Darwin Smith II <dwin@dlsmi.com>
2015-09-14 10:47:12 -0700

    Added comments

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,4 +1,5 @@
 # Bubble Sort - Darwin Smith II - 2015-9-9
+# dsjr2006 on GitHub
 import random, datetime
 theList = []
 number = 100
@@ -8,15 +9,15 @@ stop = 0
 while number > 0:
 	theList.append(random.randint(1, 1000))
 	number -= 1
-
+# Print the list created
 print 'Before:', theList ,'\n\n'	
-
+# Rearrange list in ascending numerical order
 for numerals in range(len(theList)-1,0,-1):
 	if start == 0:
-		start = datetime.datetime.now()
+		start = datetime.datetime.now() # Start Counter
         for i in range(numerals):
             if theList[i]>theList[i+1]:
                 theList[i],theList[i+1]=theList[i+1],theList[i] 
-stop =  datetime.datetime.now()
-print 'After:', theList
-print '\nArrange Time:', (stop - start)
+stop =  datetime.datetime.now() # Stop Counter
+print 'After:', theList # Print re-arranged list
+print '\nArrange Time:', (stop - start) # Print run-time
